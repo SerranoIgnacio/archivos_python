f0f2ac86b8456e5948afb8679d440e207ddf4f99
Ignacio Serrano <johndoe@example.com>
2021-12-14 18:45:41 -0300

    prac 2

--- ejercicios_practica_2.py
+++ ejercicios_practica_2.py
@@ -15,7 +15,7 @@ import csv
 def ej3():
     print('Ejercicio de archivos CSV 1º')
     archivo = 'stock.csv'
-    
+    v_stock = ''
     # Realice un programa que abra el archivo 'stock.csv'
     # en modo lectura y cuente el stock total de tornillos
     # a lo largo de todo el archivo, 
@@ -28,7 +28,16 @@ def ej3():
 
     # Comenzar aquí, recuerde el identado dentro de esta funcion
     
-
+    with open('stock.csv') as csvfile:
+        v_stock = list(csv.DictReader(csvfile))
+    torni = 0
+    for i in range(len(v_stock)):
+        lista = v_stock[i]
+        for k, v in lista.items():
+            if k == "tornillos":
+                cant_torni = v
+                torni += int(cant_torni)
+    print(f'Cantidad total es, {torni}')
 
 def ej4():
     print('Ejercicios con archivos CSV 2º')
@@ -48,6 +57,21 @@ def ej4():
 
     # Comenzar aquí, recuerde el identado dentro de esta funcion
 
+    with open('propiedades.csv') as csvfile:
+        v_prop = list(csv.DictReader(csvfile))
+
+    cantidad_dpto = len(v_prop)
+    DosAmbi = 0
+    TresAmbi = 0
+    for i in range(cantidad_dpto):
+        row = v_prop[i]
+        for k, v in row.items():
+            if k == "ambientes":
+                if "2" in v:
+                    DosAmbi += 1
+                elif "3" in v:
+                    TresAmbi += 1
+    print(f'El archivo contiene {DosAmbi} dpto con 2 ambientes y {TresAmbi} dpto con 3.')    
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
